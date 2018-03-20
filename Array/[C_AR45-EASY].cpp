5dc28ddf34dfcf5a48a51ff571ccb1de344927b5
unknown <ts0963187279@gmail.com>
2018-03-21 02:32:58 +0800

    [C_AR45-EASY] complete

---
+++ Array/[C_AR45-EASY].cpp
@@ -0,0 +1,11 @@
+/*Author: Vector Wang*/
+using namespace std;
+#include <iostream>
+
+int main(){
+  string input;
+  while(getline(cin,input)){
+    cout << "There are " << input.size() << " characters"<<endl;
+  }
+  return 0;
+}
