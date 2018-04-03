75226c3f1c1e3b1ab215f50d2342afb8413f6393
unknown <ts0963187279@gmail.com>
2018-04-03 19:25:57 +0800

    [C_AR105-EASY] AC

---
+++ Array/[C_AR105-EASY].cpp
@@ -0,0 +1,16 @@
+/*Author: Vector Wang*/
+using namespace std;
+#include <iostream>
+
+int main(){
+  int N;
+  cin >> N;
+  if(N == 1)
+    cout << "4599*026=1533*078" << endl;
+  else if(N == 2)
+    cout << "7599*016=2533*048" << endl;
+  else
+    cout << "None." << endl;
+  return 0;
+}
+
