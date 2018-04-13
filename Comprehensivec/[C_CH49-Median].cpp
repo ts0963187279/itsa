235ee538c666b0699d7a59940d37b7e41c65a936
unknown <ts0963187279@gmail.com>
2018-04-13 16:20:47 +0800

    [C_CH49-Median] AC

---
+++ Comprehensivec/[C_CH49-Median].cpp
@@ -0,0 +1,26 @@
+/*Author: Vector Wang*/
+using namespace std;
+#include <iostream>
+#include <map>
+#include <vector>
+int main(){
+  int n;
+  cin >> n;
+  vector<string> list;
+  map<string,vector<int> > price;
+  for(int i = 0 ; i < n ; i++){
+    string name;
+    int p;
+    cin >> name >> p;
+    if(price.find(name) == price.end())
+      list.push_back(name);
+    price[name].push_back(p);
+  }
+  for(int i = 0 ; i < list.size() ; i ++){
+    cout << list[i] << ",";
+    for(int j = 0 ; j < price[list[i]].size() ; j++)
+      cout << price[list[i]][j] << ",";
+    cout << endl;
+  }
+  return 0;
+}
