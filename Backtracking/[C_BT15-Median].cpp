56d7ac3fabf7038f9f219b09b0d0ecc652883636
unknown <ts0963187279@gmail.com>
2018-04-29 19:39:17 +0800

    [C_BT15-Median] update

--- Backtracking/[C_BT15-Median].cpp
+++ Backtracking/[C_BT15-Median].cpp
@@ -3,15 +3,19 @@ using namespace std;
 #include <iostream>
 #include <vector>
 #include <map>
-int find_place(vector<vector<int> > &game , int n , int N , int x, int y , map<vector<vector<int> > , int> &table);
+int find_place(vector<vector<int> > &lines ,int n ,int N,int x);
 int main(){
   int n;
   while(cin >> n){
     map<vector<vector<int> > , int> table;
-    vector<vector<int> > game(n,vector<int>(n,0));
+    vector<vector<int> > lines(3);
+    lines[0].resize(n,0);
+    lines[1].resize(n*2,0);
+    lines[2].resize(n*2,0);
     int x,y;
     cin >> x >> y;
-    int vaild = find_place(game,0,n,x,y,table);
+    lines[0][y] = lines[1][x + y] = lines[2][x - y + n] = 1;
+    int vaild = find_place(lines,0,n,x);
     if(vaild > 0)
       cout << "YES(" << vaild << ")" << endl;
     else
@@ -19,36 +23,19 @@ int main(){
   }
   return 0;
 }
-int find_place(vector<vector<int> > &game , int n , int N ,int x, int y,map<vector<vector<int> > , int> &table){
-  vector<int> dx = {0,-1,0,1,1,1,-1,-1}, dy = {-1,0,1,0,1,-1,1,-1};
-  game[x][y] ++;
-  for(int i = 0 ; i < 8 ; i++){
-    int tmp_x = x, tmp_y = y;
-    while(tmp_x + dx[i] >= 0 && tmp_x + dx[i] < game.size() && tmp_y + dy[i] >= 0 && tmp_y + dy[i] < game.size()){
-      tmp_x += dx[i];
-      tmp_y += dy[i];
-      game[tmp_x][tmp_y]++;
-    }
-  }
+int find_place(vector<vector<int> > &lines ,int n ,int N,int x){
   int sum = 0;
-  if(n == N - 1 && table.find(game) == table.end()){
-    table[game] = 1;
+  if(n == x)
+    n ++;
+  if(n >= N){
     sum ++;
-  }
-  for(int i = 0 ; i < N ; i ++){
-    for(int j = 0 ; j < N ; j ++){
-      if(game[i][j] == 0){
-	sum += find_place(game,n + 1,N,i,j,table);
+  }else{
+    for(int i = 0 ; i < N ; i++){
+      if(lines[0][i] == 0 && lines[1][n + i] == 0 && lines[2][n - i + N] == 0){
+	lines[0][i] = lines[1][n + i] = lines[2][n - i + N] = 1;
+	sum += find_place(lines,n + 1,N , x);
+	lines[0][i] = lines[1][n + i] = lines[2][n - i + N] = 0;
       }
-    }
-  }
-  game[x][y] --;
-  for(int i = 0 ; i < 8 ; i++){
-    int tmp_x = x, tmp_y = y;
-    while(tmp_x + dx[i] >= 0 && tmp_x + dx[i] < game.size() && tmp_y + dy[i] >= 0 && tmp_y + dy[i] < game.size()){
-      tmp_x += dx[i];
-      tmp_y += dy[i];
-      game[tmp_x][tmp_y]--;
     }
   }
   return sum;
